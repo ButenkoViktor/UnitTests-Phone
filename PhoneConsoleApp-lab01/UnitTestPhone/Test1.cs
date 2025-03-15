d201d594184188eb8a251c3cb9cba513616813af
Viktor Butenko <viktor.butenko@microsoft.wsei.edu.pl>
2025-03-15 11:09:25 +0100

    1 test wykonany na lab

--- PhoneConsoleApp-lab01/UnitTestPhone/Test1.cs
+++ PhoneConsoleApp-lab01/UnitTestPhone/Test1.cs
@@ -4,9 +4,25 @@ namespace UnitTestPhone
     [TestClass]
     public sealed class Test1
     {
-        [TestMethod]
-        public void TestMethod1()
+        /// Test 1 **Lab** 
+        [TestClass]
+        public class TestPhone
         {
+            [TestMethod]
+            public void Test_konstruktor_dane_poprawne_Działanie_ok()
+            {
+                ///AAA
+                ///Arrange
+                var wlasciciel = "Butenko";
+                var numerTelefonu = "123456789";
+                ///act
+                var phone = new Phone(wlasciciel, numerTelefonu);
+
+                ///assert
+                Assert.AreEqual(wlasciciel, phone.Owner);
+                Assert.AreEqual(numerTelefonu, phone.PhoneNumber);
+            }
         }
     }
 }
+
