23ad04191de555430ca7a586ddd7d9be96fb6ed1
Viktor Butenko <viktor.butenko@microsoft.wsei.edu.pl>
2025-03-15 11:05:23 +0100

    dodawanie klasy Phone|unitTest

---
+++ PhoneConsoleApp-lab01/UnitTestPhone/Test1.cs
@@ -0,0 +1,12 @@
+﻿using ClassLibrary;
+namespace UnitTestPhone
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
