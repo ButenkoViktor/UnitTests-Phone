df55132c20dee98f3d5fa9c3ebc82425946c2687
Viktor Butenko <viktor.butenko@microsoft.wsei.edu.pl>
2025-03-15 11:19:28 +0100

    Jeszcze 4 testy

--- PhoneConsoleApp-lab01/UnitTestPhone/Test1.cs
+++ PhoneConsoleApp-lab01/UnitTestPhone/Test1.cs
@@ -2,27 +2,51 @@
 namespace UnitTestPhone
 {
     [TestClass]
-    public sealed class Test1
+    public class TestPhone
     {
         /// Test 1 **Lab** 
-        [TestClass]
-        public class TestPhone
+        [TestMethod]
+        public void Test_konstruktor_dane_poprawne_Działanie_ok()
         {
-            [TestMethod]
-            public void Test_konstruktor_dane_poprawne_Działanie_ok()
-            {
-                ///AAA
-                ///Arrange
-                var wlasciciel = "Butenko";
-                var numerTelefonu = "123456789";
-                ///act
-                var phone = new Phone(wlasciciel, numerTelefonu);
+            ///AAA
+            ///Arrange
+            var wlasciciel = "Butenko";
+            var numerTelefonu = "123456789";
+            ///act
+            var phone = new Phone(wlasciciel, numerTelefonu);
 
-                ///assert
-                Assert.AreEqual(wlasciciel, phone.Owner);
-                Assert.AreEqual(numerTelefonu, phone.PhoneNumber);
-            }
+            ///assert
+            Assert.AreEqual(wlasciciel, phone.Owner);
+            Assert.AreEqual(numerTelefonu, phone.PhoneNumber);
         }
+
+        /// Test 2 **Sprawdza, czy konstruktor rzuca wyjątek, gdy właściciel jest null.**"
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_konstruktor_wlasciciel_null()
+        {
+            ///AAA
+            ///Arrange
+            var numerTelefonu = "123456789";
+            ///act
+            var phone = new Phone(null, numerTelefonu);
+        }
+
+        /// Test 3 **Sprawdza, czy konstruktor rzuca wyjątek, gdy numer telefonu jest nieprawidłowy.**
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_Konstruktor_NieprawidlowyNumerTelefonu()
+        {
+            ///AAA
+            ///Arrange
+            var wlasciciel = "Butenko";
+            var numerTelefonu = "12345678";
+            ///act
+            var phone = new Phone(wlasciciel, numerTelefonu);
+        }
+
+
+
     }
 }
 
